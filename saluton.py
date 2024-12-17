366aa7c22884a8cf703c4db6fe983ff18aeadbf5
Kiaan <kiaan24@tutanota.com>
2024-12-17 19:38:59 +0000

    Create saluton.py

---
+++ saluton.py
@@ -0,0 +1,19 @@
+print("Saluton! Kiel vi nomiĝas?")
+nomo = input()
+print("Mi ŝatas ",nomo,". Ĝi estas bonan nomon!")
+print("Kio estas vian favoratan koloron?")
+koloro = input()
+if koloro == "ruĝa":
+  print("Mi ne povas parolas")
+elif koloro == "oranĝa":
+  print("Mi tre ŝatas ĉi tiun koloron!")
+elif koloro == "flava":
+  print("Ĉi tiu koloro estas aĉa.")
+elif koloro == "verda":
+  print("Verda - la koloro de fungoj - ĝi estas naŭza!")
+elif koloro == "blua":
+  print("Ĉi tiu koloro estas aĉa.")
+elif koloro == "violkolora":
+  print("Ĉi tiu koloro estas aĉa.")
+else:
+  print(koloro,"estas mia favoratan koloron(!)")

7850785ec2cf1979c3c8d06e8cec6820bd89e357
Kiaan <kiaan24@tutanota.com>
2025-01-23 19:13:09 +0000

    Update saluton.py

--- saluton.py
+++ saluton.py
@@ -1,10 +1,22 @@
+def viJaFurzis():
+    print("Kial vi pensis, ke mi furzis?")
+    eniri1 = input()
+    if eniri1 == "Mi ne scias":
+        print("Vi ja scias, sed vi ne parolas ĝin al mi!")
+    else:
+        print("Bone, foriru")
+
 print("Saluton! Kiel vi nomiĝas?")
 nomo = input()
-print("Mi ŝatas ",nomo,". Ĝi estas bonan nomon!")
-print("Kio estas vian favoratan koloron?")
+if nomo == "Kial vi furzis?":
+    print("Mi neniam furzas")
+else:
+    print("Mi ŝatas "+nomo+". Ĝi estas bona nomo!")
+
+print("Kio estas via favorata koloro?")
 koloro = input()
 if koloro == "ruĝa":
-  print("Mi ne povas parolas")
+  print("Mi ne povas paroli")
 elif koloro == "oranĝa":
   print("Mi tre ŝatas ĉi tiun koloron!")
 elif koloro == "flava":
@@ -15,5 +27,8 @@ elif koloro == "blua":
   print("Ĉi tiu koloro estas aĉa.")
 elif koloro == "violkolora":
   print("Ĉi tiu koloro estas aĉa.")
+elif koloro == "Vi ja furzis":
+    print("Neniel! Mi ne furzis")
+    viJaFurzis()
 else:
-  print(koloro,"estas mia favoratan koloron(!)")
+  print(koloro,"estas mia favorata koloro(!)")
